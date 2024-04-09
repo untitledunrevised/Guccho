7470f4ce232fcd06db6a1aae872e8c2d069950c9
arily <arily@arily.moe>
2024-04-09 09:21:05 +0800

    delete old tokens

--- src/server/backend/$base/server/mail-token.ts
+++ src/server/backend/$base/server/mail-token.ts
@@ -20,7 +20,8 @@ export abstract class MailTokenProvider {
   abstract getOrCreate(email: MailTokenProvider.Email): Promise<{ otp: MailTokenProvider.OTP; token: MailTokenProvider.Token }>
   abstract get(input: MailTokenProvider.Validation): Promise<{ otp: MailTokenProvider.OTP; token: MailTokenProvider.Token; email: MailTokenProvider.Email } | undefined>
   abstract getByEmail(email: MailTokenProvider.Email): Promise<{ otp: MailTokenProvider.OTP; token: MailTokenProvider.Token } | undefined>
-  abstract delete(email: MailTokenProvider.Email): Promise<void>
+  abstract delete(input: MailTokenProvider.Validation): Promise<void>
+  abstract deleteAll(email: MailTokenProvider.Email): Promise<void>
 
   protected generateOTP(): MailTokenProvider.OTP {
     return Math.floor(Math.random() * 900000 + 100000).toString() as MailTokenProvider.OTP
